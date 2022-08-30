af78883393994d93d10357c118967b3899b78911
gymbundye <gymbundye@gmail.com>
2022-08-30 12:41:11 -0400

    chose headshot

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import headshot from '/public/Images/headshot.jpg'
 
 function About() {
   return (
     <div>
-        <image />
+        <img src={headshot} className = 'headshot'/>
         <h1>James Bundy</h1>
     </div>
   )
