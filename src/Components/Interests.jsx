0ca3356ef383e770e2525c5e41c5ec4d5e64e869
gymbundye <gymbundye@gmail.com>
2022-09-07 21:42:09 -0400

    completed styling

--- src/Components/Interests.jsx
+++ src/Components/Interests.jsx
@@ -4,7 +4,7 @@ function Interests() {
   return (
     <div>
         <h6>Interests</h6>
-        <p className='about'>Family First! I have two great kids
+        <p className='interests'>Family First! I have two great kids
         that I love to spend time with and a wife that tolerates me! I love movies. 
         The scarier the better. I have been known to play a video game a time or two!
         Fan of all forms of Pizza.
