918d8a1df71ef1257dcf177da8ed6ac9fbaf88d6
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-07-08 17:50:37 -0400

    changed about copy and some colors

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,9 +4,10 @@ function About() {
   return (
     <div>
       <h6>About</h6>
-      <p className='about'>Recent Bootcamp graduate looking to join a 
-      great team! Always trying to learn new things and technologies. 
-      Charismatic, with many years of Customer Service. Let me help build your next project!
+      <p className='about'>Highly motivated, creative developer looking to contribute to an organization that is solving the most important
+problems. I have a high level of detail and am mission driven. I’m a charismatic problem solver who spent his life in
+customer service and now wants to make a difference within the world of technology.
+
       </p>
     </div>
   )
---
+++ src/Components/Email.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Email() {
+  return (
+    <div>
+        <form>
+            <input type='text'></input>
+        </form>
+    </div>
+  )
+}
+
+export default Email
